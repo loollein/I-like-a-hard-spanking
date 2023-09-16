8e86e15114b144d3bf6a301a0536e2afda6ae9a3
Danil <74613455+loollein@users.noreply.github.com>
2023-09-16 16:33:27 +0700

    I did my homework on checking a number for parity.
    Homework from the video course, did it, I'm very happy =)

--- firstProject/firstProject/main.cpp
+++ firstProject/firstProject/main.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 void main()
 {
-	cout << "\t\t\tWhat is this life if, full of care,\n\t\t\tWe have no time to stand and stare.\n\n\t\t\tNo time to stand beneath the boughs\n\t\t\tAnd stare as long as sheep or cows.\n\n\t\t\tNo time to see, when woods we pass,\n\t\t\tWhere squirrels hide their nuts in grass.\n\n\t\t\t\No time to see, in broad daylight,\n\t\t\tStreams full of stars, like skies at night.\n\n\t\t\tNo time to turn at Beauty’s glance,\n\t\t\tAnd watch her feet, how they can dance.\n\t\t\tNo time to wait till her mouth can\n\t\t\tEnrich that smile her eyes began.\n\n\t\t\tA poor life this if, full of care,\n\t\t\tWe have no time to stand and stare.";
+	int a;
+
+	cout << "Enter the number: ";
+	cin >> a;
+
+	if (a%2 == 0)
+	{
+		cout << "Even number";
+	}
+	else
+	{
+		cout << "Odd number";
+	}
 }
